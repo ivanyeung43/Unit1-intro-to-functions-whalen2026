fc3f2bf64df42db5eb32b382010a229a8b60a8ac
ivanyeung43 <ivany43@nycstudents.net>
2026-01-28 09:24:34 -0500

    Initial Day

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+
+
+
+def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(90)
+turtle.done()
