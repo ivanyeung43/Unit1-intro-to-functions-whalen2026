dee1e7c84322b4d32737a22dc734e11fc9a34577
ivanyeung43 <ivany43@nycstudents.net>
2026-01-30 09:26:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,21 @@ import turtle
 from turtle import *
 t = Turtle()
 t.shape('turtle')
+t.speed(10)
 sidelength = 100
-rotate = 90
+rotate = 144
 def square(sidelength,rotate):
-    for i in range(4):
+    for i in range(5):
         t.forward(sidelength)
         t.left(rotate)
 
 def addSquares(iRange):
-    length = 25
+    length = 20
     for i in range(iRange):
         square(length, rotate)
         length += 5
-        t.right(5)
-addSquares(60)
+        t.right(10)
+addSquares(120)
 
 
 
